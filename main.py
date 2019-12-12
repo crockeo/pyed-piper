951e3d65bd7931971fc6c8feee1ddaa4d79154f4
Cerek Hillen <cerekh@gmail.com>
2019-12-12 12:47:47 -0500

    Generating overtones

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class ToneGenerator:
     def __init__(self, frequency: float):
         self.frequency = frequency
 
-    def generate_tone(self):  # TODO: Find numpy type
+    def generate_tone(self) -> np.ndarray:
         return np.sin(
             2 * np.pi * np.arange(SAMPLE_RATE * DURATION) * self.frequency / SAMPLE_RATE
         ).astype(np.float32)
@@ -21,7 +21,7 @@ class CompositeToneGenerator:
     def __init__(self, tone_generators: List[ToneGenerator]):
         self.tone_generators = tone_generators
 
-    def generate_tone(self):  # TODO: Find numpy type
+    def generate_tone(self) -> np.ndarray:
         sample = np.zeros(int(SAMPLE_RATE * DURATION))
         for tone_generator in self.tone_generators:
             tone = tone_generator.generate_tone()
@@ -30,12 +30,23 @@ class CompositeToneGenerator:
         return sample.astype(np.float32)
 
 
+class OverToneGenerator:
+    def  __init__(self, base_frequency: float):
+        self.composite = CompositeToneGenerator([
+            ToneGenerator(base_frequency * (1 + overtone))
+            for overtone in range(7)
+        ])
+
+    def generate_tone(self) -> np.ndarray:
+        return self.composite.generate_tone()
+
+
 def main():
     chord = CompositeToneGenerator(
         [
-            ToneGenerator(440),  # A4
-            ToneGenerator(493.88),  # B4
-            ToneGenerator(523.25),  # C5
+            OverToneGenerator(440),  # A4
+            OverToneGenerator(493.88),  # B4
+            OverToneGenerator(523.25),  # C5
         ]
     )
 
