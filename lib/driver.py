2b8b0d4b4e799a562220fea633ddee9bbf96bffd
Cerek Hillen <cerekh@gmail.com>
2019-12-14 16:40:26 -0500

    Removed popping at the end

--- lib/driver.py
+++ lib/driver.py
@@ -37,25 +37,47 @@ class ToneDriver(BaseDriver):
 
         self.start_time = 0
         self.running = False
+        self.zeroed = True
 
     def start(self, time: float):
         self.start_time = time
         self.running = True
+        self.zeroed = False
 
     def stop(self):
         self.running = False
 
     def is_running(self):
-        return self.running
+        return self.running or not self.zeroed
 
     def generate_frame(self, time: float, frame_count: int) -> np.ndarray:
         if not self.is_running():
             return np.zeros(frame_count).astype(np.float32)
 
         frame_begin_time = time - self.start_time
-        return self.amplitude * np.sin(
+        wave = self.amplitude * np.sin(
             2
             * np.pi
             * self.frequency
-            * (frame_begin_time + (np.arange(frame_count)) / self.sample_rate),
-        ).astype(np.float32)
+            * (frame_begin_time + (np.arange(frame_count)) / self.sample_rate)
+        )
+
+        # TODO: Find out how to zero without having to loop through the entire
+        #       wave. It's only ever 1024 frames wide, but still not great.
+        if not self.running and not self.zeroed:
+            # If we're trying to stop, first we need to generate enough of the
+            # tone to zero out the wave. Otherwise you hear a pop when you
+            # release a button.
+            boundary = -1
+            for i in range(len(wave) - 1):
+                if (wave[i] <= 0 and wave[i + 1] > 0) or (
+                    wave[i] >= 0 and wave[i + 1] < 0
+                ):
+                    boundary = i
+                    break
+
+            if boundary > 0:
+                wave[boundary + 1 :] = np.zeros(len(wave) - boundary - 1)
+                self.zeroed = True
+
+        return wave.astype(np.float32)
