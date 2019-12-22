0b01e1ecc019c1229972d54e6f3aac279755a435
Cerek Hillen <cerekh@gmail.com>
2019-12-21 19:36:43 -0500

    SVGs!

--- web-ui/src/components/App.tsx
+++ web-ui/src/components/App.tsx
@@ -3,14 +3,7 @@ import React from "react";
 import logo from "../res/logo.svg";
 import "../styles/App.css";
 
-import SynthButton from "./SynthButton";
-
 const App: React.FC = () => {
-  const synthButtons = [];
-  for (let i = 0; i < 16; i++) {
-    synthButtons.push(<SynthButton key={i} buttonIndex={i} />);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
@@ -21,8 +14,6 @@ const App: React.FC = () => {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-
-        {synthButtons}
       </header>
     </div>
   );
