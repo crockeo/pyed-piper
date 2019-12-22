fce16a9e3a7b1e1e12d74b257f57914f988e6822
Cerek Hillen <cerekh@gmail.com>
2019-12-22 09:56:59 -0500

    Top bar

--- web-ui/src/components/App.tsx
+++ web-ui/src/components/App.tsx
@@ -6,15 +6,12 @@ import "../styles/App.scss";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <span className="title">Pyed Piper</span>
+      </div>
+
+      <div className="body">TODO</div>
     </div>
   );
 };
